8cba6d3351a2d5e3f125f9af08ccc3d3201736a5
Alessandro Fadini <aleksfadini@gmail.com>
2019-09-27 23:48:08 -0400

    still not quite right

--- Scenes/Node2D.gd
+++ Scenes/Node2D.gd
@@ -8,7 +8,7 @@ var first_press = true
 var counter = 0
 var beat_in_ms = 0
 # Bar and beat counters
-var bar_counter = 0
+var bar_counter = 1 # the first bar is called "1"
 var beat_counter = 0
 var beats_per_bar = 4
 #Loops
@@ -35,26 +35,11 @@ func _ready():
 	OtherBeat = $OtherBeat
 	# initialize buffer system
 	last_beat_buffer_delay = look_ahead
-	# Connect Functions for Beats
-#	FirstBeat.connect("finished",self,"_on_each_beat_finished")
-#	FirstBeat.connect("finished",self,"_on_each_beat_finished")
-#	OtherBeat.connect("finished",self,"_on_each_beat_finished")
-#	OtherBeat.connect("finished",self,"_on_each_beat_finished")
-#	OtherBeat.connect("finished",OtherBeat,"_on_each_beat_finished")
-#	connect("_on_each_beat_finished",OtherBeat,"finished")
-#	var timeline_file = load('res://audio/A0.ogg')
-#	$Timeline.set_volume_db(-80)
-#	$Timeline.set_stream(timeline_file)
-#	audiostream.set_loop(true)
-#	$Timeline.play(0)
-
-
 	
 func _process(delta):
 	# These two are needed to detect BPM
 	time_elapsed += delta
 	counter += delta
-#
 #	while counter >= 0.005 and false:
 #		counter -= 0.005
 #		$AudioStreamPlayer.play()
@@ -64,15 +49,9 @@ func _process(delta):
 
 func check_timeline():
 	# find how close this frame is to the next beat
-	var ms_from_beat = beat_in_ms-(OS.get_ticks_msec()% beat_in_ms)
+#	var ms_from_beat = beat_in_ms-(OS.get_ticks_msec()% beat_in_ms)
+	var ms_from_beat = (OS.get_ticks_msec()% beat_in_ms)
 #	print("GT: ",ms_from_beat)
-	# if closer than look_ahead and not already played, play with delay
-	if ms_from_beat < look_ahead and not beat_already_played:
-		beat_already_played = true
-		play_with_delay(ms_from_beat)
-		#set the buffer delayed to use as reference
-		last_beat_buffer_delay = ms_from_beat
-		print("last_beat_buffer_delay: ", last_beat_buffer_delay) 
 	# check if beat has already been played
 	if ms_from_beat <= last_beat_buffer_delay:
 #		beat_already_played = true
@@ -80,13 +59,13 @@ func check_timeline():
 	else:
 		beat_already_played = false
 		last_beat_buffer_delay = look_ahead
-    # do something every 300ms
-	pass
-# Insert random events here
-func check_thresholds():
-	if counter > 5 and not(5 in blacklist):
-		blacklist.append(5)
-		print("HI:D")
+	# if closer than look_ahead and not already played, play with delay
+	if ms_from_beat < look_ahead and not beat_already_played:
+		play_with_delay(ms_from_beat)
+		beat_already_played = true
+		#set the buffer delayed to use as reference
+		last_beat_buffer_delay = ms_from_beat
+		print("last_beat_buffer_delay: ", last_beat_buffer_delay) 
 	
 # detect bpm on button pressed
 func _on_Button_pressed():
@@ -122,32 +101,12 @@ func _on_first_press_timer_timeout():
 #func play_sound():
 #	$AudioStreamPlayer.play()
 	
-#### Requires a recorded stream set at 60bpm
-func morph_beat_loop_to_given_bpm(audiostreamNode,target_bpm):
-	# calculate pitch shift required (warp)
-	var warp_required = 1
-	var stretch_relative_to_120_bpm = float(target_bpm)/120
-	warp_required = stretch_relative_to_120_bpm
-	# shift pitch (warp)
-	audiostreamNode.pitch_scale = warp_required
-	# calculate compensation in octaves
-	var pitch_compensation = 1
-	# Hopefully the same? If speed doubles, octaves lowers
-	# sadly pitch shift goes from 0 (-1 oct) to 16 (+16 oct)
-	pitch_compensation = float(1)/warp_required
-	# define compensation bus
-	var compensation_bus = AudioServer.get_bus_index("PComp")
-	# add audioNode to the bus
-	audiostreamNode.bus="PComp"
-	# compensate with pitch shift effect on the bus
-#	#AudioServer.set_pitch_scale(compensation_bus, pitch_compensation)
-	var pitch_effect = AudioServer.get_bus_effect(1, 0)
-	pitch_effect.pitch_scale=pitch_compensation
-	pass
+
 	
 # This function keeps triggering one beat after 
 # the other.
 func play_with_delay(delay):
+	# note: this division introduces an error. check float/integers
 	var delay_in_secs=float(delay)/1000
 	print ("playing with delay: ", delay_in_secs)
 	beat_counter+=1
@@ -184,3 +143,37 @@ func bpm_to_beat_in_ms(any_bpm):
 	var beat_in_ms = float(6000)/any_bpm
 	print ("calc - bpm: ", any_bpm, "to ms: ", beat_in_ms)
 	return int(beat_in_ms)
+	
+
+#################################################33333
+############### TROLLING SECTION
+###################################
+
+# Insert random events here
+func check_thresholds():
+	if counter > 5 and not(5 in blacklist):
+		blacklist.append(5)
+		print("HI:D")
+	
+#### Requires a recorded stream set at 60bpm
+func morph_beat_loop_to_given_bpm(audiostreamNode,target_bpm):
+	# calculate pitch shift required (warp)
+	var warp_required = 1
+	var stretch_relative_to_120_bpm = float(target_bpm)/120
+	warp_required = stretch_relative_to_120_bpm
+	# shift pitch (warp)
+	audiostreamNode.pitch_scale = warp_required
+	# calculate compensation in octaves
+	var pitch_compensation = 1
+	# Hopefully the same? If speed doubles, octaves lowers
+	# sadly pitch shift goes from 0 (-1 oct) to 16 (+16 oct)
+	pitch_compensation = float(1)/warp_required
+	# define compensation bus
+	var compensation_bus = AudioServer.get_bus_index("PComp")
+	# add audioNode to the bus
+	audiostreamNode.bus="PComp"
+	# compensate with pitch shift effect on the bus
+#	#AudioServer.set_pitch_scale(compensation_bus, pitch_compensation)
+	var pitch_effect = AudioServer.get_bus_effect(1, 0)
+	pitch_effect.pitch_scale=pitch_compensation
+	pass
